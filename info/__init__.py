4938e90da55effc1730b598cb0bb004a97416269
gebaichen <luyujie19831217@gmail.com>
2022-02-08 17:33:56 +0800

    '1.3使用蓝图模块化程序'

--- info/__init__.py
+++ info/__init__.py
@@ -3,9 +3,10 @@ import os
 from flask import Flask
 
 from configs import config
-from extensions import db,migrate,login_manager,redis_store
+from extensions import db, migrate, login_manager, redis_store
 from common.utils import setup_log
 import pymysql
+
 
 def create_app(config_name=None):
     pymysql.install_as_MySQLdb()
@@ -19,12 +20,19 @@ def create_app(config_name=None):
 
     # 注册插件
     register_extensions(app)
-
+    register_blueprint(app)
     return app
 
 
 def register_extensions(app):
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     redis_store.init_app(app)
+
+
+def register_blueprint(app: Flask):
+    from info.index import index_bp
+    from info.auth import auth_bp
+    app.register_blueprint(index_bp)
+    app.register_blueprint(auth_bp)
